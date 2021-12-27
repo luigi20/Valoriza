05ecc6e688fce51f5159a6cd015fd734a4c537bc
Luis <luisopentec@gmail.com>
2021-12-27 15:58:31 -0300

    handling errors

---
+++ src/middlewares/Errors.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
+class ErrosApp {
+
+    async execute(err: Error, req: Request, res: Response, next: NextFunction) {
+        if (err instanceof Error) {
+            return res.status(400).json({ error: err.message });
+        }
+        return res.status(500).json({
+            status: err,
+            message: "Internal Server Error"
+        });
+    }
+}
+
+export { ErrosApp };
+
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,23 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import 'reflect-metadata';
 import './database';
 import { router } from './routes';
+import { ErrosApp } from './middlewares/Errors';
+
+const errors = new ErrosApp();
 
 const app = express();
 app.use(express.json());
 app.use(router);
+app.use(errors.execute);
+/*app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return res.status(400).json({ error: err.message });
+    }
+    return res.status(500).json({
+        status: err,
+        message: "Internal Server Error"
+    });
+})*/
 app.listen(3000);
